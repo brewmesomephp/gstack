1ea87faeabd53ac94bf9427e0d60a84f2603c11e
Joseph Alai <josephalai@gmail.com>
2017-08-30 20:37:00 -0700

    When Company accepts application, it adds to database now the worker (company_worker_invites) or whatve

--- process_add_worker.php
+++ process_add_worker.php
@@ -7,7 +7,18 @@ include_once "functions.php";
 $dbs = db_connection();
 
 if (isset($_GET['add_game'])){
-    print_r($_POST);
+    $_JSON = $_POST;
+    $invite = array_to_object($_JSON);
+//    print_r($_POST);
+    $q = "INSERT INTO `company_worker_invites`
+    (`fromid`, `toid`, `added`, `games`, `job_title`)
+    VALUES ('$invite->companyid', '$invite->userid', 'CURRENT_TIMESTAMP', '$invite->game_id', '$invite->jobtitle');";
+    
+    
+    $sql = $dbs->prepare($q);
+    $sql->execute();
+    
+    print "Invitation Sent to User";
 }
 
 ?>
